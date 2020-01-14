e93ba73afa4e8d7786aa72ef9181291d85826caf
JEAN PAUL PICHON <jpaul_pichon@hotmail.com>
2020-01-14 13:53:33 +0100

    COMMIT FIRST COMMENT

--- src/tp/NewMain1.java
+++ src/tp/NewMain1.java
@@ -15,7 +15,10 @@ public class NewMain1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        /*
+        FIRST COMMENT
+        
+        */
     }
     
 }
